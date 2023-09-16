2a9a3e514d5ece8f72655c2e123793ad7202144f
jerlenem <biktagirovainur20@gmail.com>
2023-09-16 16:43:51 +0300

    fix tests

--- src/test/java/ru/sber/beautifulcode/service/BracketCheckerServiceTest.java
+++ src/test/java/ru/sber/beautifulcode/service/BracketCheckerServiceTest.java
@@ -12,103 +12,104 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class BracketCheckerServiceTest {
 
-  @InjectMocks private BracketCheckerService checkerService;
+    @InjectMocks
+    private BracketCheckerService checkerService;
 
-  @BeforeEach
-  public void setUp() {
-    MockitoAnnotations.openMocks(this);
-  }
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-  @Test
-  public void checkerBracket_IsBracketPlacementCorrect_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_IsBracketPlacementCorrect_ReturnsValidValue() {
 
-    String correctText =
-        """
-                    {
-                     Вчера я отправился в поход в лес (это мое любимое место для отдыха)\
-                     вместе с друзьями. Мы выбрали маршрут, который проходил через\
-                     горные потоки и поля (для разнообразия). В начале пути погода была\
-                     отличной, солнце светило ярко, и птицы радостно пели. Однако, когда\
-                     мы подошли ближе к вершине горы, небо стало покрываться облаками,\
-                     (как будто природа готовила нам небольшой сюрприз). Несмотря на это,\
-                     виды были захватывающими, особенно когда мы достигли высшей точки и\
-                     увидели прекрасный вид на долину (я почувствовал, что все усилия стоили того)."\
-                     }
-                     """;
+        String correctText =
+                """
+                        {
+                         Вчера я отправился в поход в лес (это мое любимое место для отдыха)\
+                         вместе с друзьями. Мы выбрали маршрут, который проходил через\
+                         горные потоки и поля (для разнообразия). В начале пути погода была\
+                         отличной, солнце светило ярко, и птицы радостно пели. Однако, когда\
+                         мы подошли ближе к вершине горы, небо стало покрываться облаками,\
+                         (как будто природа готовила нам небольшой сюрприз). Несмотря на это,\
+                         виды были захватывающими, особенно когда мы достигли высшей точки и\
+                         увидели прекрасный вид на долину (я почувствовал, что все усилия стоили того)."\
+                         }
+                         """;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertTrue(value);
-  }
+        assertTrue(value);
+    }
 
-  @Test
-  public void checkerBracket_IsBracketPlacementIncorrect_ReturnsInvalidValue() {
+    @Test
+    public void checkerBracket_IsBracketPlacementIncorrect_ReturnsInvalidValue() {
 
-    String incorrectText =
-        """
+        String incorrectText =
+                """
                         Некорректный текст для проверки (аывавыа]
                         """;
 
-    var value = checkerService.isCorrect(incorrectText);
+        var value = checkerService.isCorrect(incorrectText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_IsNestedBracketsPlacementCorrect_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_IsNestedBracketsPlacementCorrect_ReturnsValidValue() {
 
-    String correctText =
-        """
-                    Верный (текст с (правильными (которые обязательно должны быть успешными)) скобками).
-                    """;
+        String correctText =
+                """
+                        Верный (текст с (правильными (которые обязательно должны быть успешными)) скобками).
+                        """;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertTrue(value);
-  }
+        assertTrue(value);
+    }
 
-  @Test
-  public void checkerBracket_IsNestedBracketsPlacementIncorrect_ReturnsInvalidValue() {
+    @Test
+    public void checkerBracket_IsNestedBracketsPlacementIncorrect_ReturnsInvalidValue() {
 
-    String incorrectText =
-        """
-                    Неверный (текст с (неправильными скобками [которые не должны быть успешными))).
-                    """;
-    var value = checkerService.isCorrect(incorrectText);
+        String incorrectText =
+                """
+                        Неверный (текст с (неправильными скобками [которые не должны быть успешными))).
+                        """;
+        var value = checkerService.isCorrect(incorrectText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_IsEmptyValueInsideBrackets_ReturnsInvalidValue() {
+    @Test
+    public void checkerBracket_IsEmptyValueInsideBrackets_ReturnsInvalidValue() {
 
-    String incorrectText = """
-            Верный (текст с пустыми(    )) скобками.
-            """;
+        String incorrectText = """
+                Верный (текст с пустыми(    )) скобками.
+                """;
 
-    var value = checkerService.isCorrect(incorrectText);
+        var value = checkerService.isCorrect(incorrectText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_ForFloatingPointNumber_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_ForFloatingPointNumber_ReturnsValidValue() {
 
-    String incorrectText =
-        """
-                    Верный (текст с пустыми(223.125121)) скобками).
-                    """;
+        String incorrectText =
+                """
+                        Верный (текст с пустыми(223.125121)) скобками).
+                        """;
 
-    var value = checkerService.isCorrect(incorrectText);
+        var value = checkerService.isCorrect(incorrectText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_LongTextWithBrackets_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_LongTextWithBrackets_ReturnsValidValue() {
 
-    String correctText =
-        """
+        String correctText =
+                """
                         Вчера я отправился в поход в лес (это мое любимое место для отдыха)
                         вместе с друзьями. Мы выбрали маршрут, который проходил через
                         горные потоки и поля (для разнообразия). В начале пути погода была
@@ -119,16 +120,16 @@ class BracketCheckerServiceTest {
                         увидели прекрасный вид на долину (я почувствовал, что все усилия стоили того).
                         """;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertTrue(value);
-  }
+        assertTrue(value);
+    }
 
-  @Test
-  public void checkerBracket_LongTextWithBrackets2_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_LongTextWithBrackets2_ReturnsValidValue() {
 
-    String correctText =
-        """
+        String correctText =
+                """
                         На горизонте (где небо сливается с морем) сияло яркое солнце. Оно казалось
                         (даже через солнечные очки) ослепительным и невероятно красивым. Море,
                         распростершееся передо мной, представляло собой бескрайнюю гладь
@@ -136,47 +137,47 @@ class BracketCheckerServiceTest {
                         приносили аромат соли и свободы.
                         """;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertTrue(value);
-  }
+        assertTrue(value);
+    }
 
-  @Test
-  public void checkerBracket_NestedBracketsText_ReturnsValidValue() {
+    @Test
+    public void checkerBracket_NestedBracketsText_ReturnsValidValue() {
 
-    String correctText =
-        """
+        String correctText =
+                """
                         (((Вложенные скобки (ещё вложенные скобки))))
                         """;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertTrue(value);
-  }
+        assertTrue(value);
+    }
 
-  @Test
-  public void checkerBracket_IsEmptyText_ReturnsInvalidValue() {
-    String incorrectText = "";
+    @Test
+    public void checkerBracket_IsEmptyText_ReturnsInvalidValue() {
+        String incorrectText = "";
 
-    var value = checkerService.isCorrect(incorrectText);
+        var value = checkerService.isCorrect(incorrectText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_IsNullableText_ReturnsInvalidValue() {
-    String correctText = null;
+    @Test
+    public void checkerBracket_IsNullableText_ReturnsInvalidValue() {
+        String correctText = null;
 
-    var value = checkerService.isCorrect(correctText);
+        var value = checkerService.isCorrect(correctText);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 
-  @Test
-  public void checkerBracket_IsNullableArgument_ReturnsInvalidValue() {
+    @Test
+    public void checkerBracket_IsNullableArgument_ReturnsInvalidValue() {
 
-    var value = checkerService.isCorrect(null);
+        var value = checkerService.isCorrect(null);
 
-    assertFalse(value);
-  }
+        assertFalse(value);
+    }
 }
